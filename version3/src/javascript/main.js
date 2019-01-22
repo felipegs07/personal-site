62feb83d87b905421ef43acb67cc81e409e4bdee
Felipe <felipegdas07@gmail.com>
2019-01-22 11:55:45 -0200

    scroll button added

--- version3/src/javascript/main.js
+++ version3/src/javascript/main.js
@@ -2,6 +2,7 @@ window.addEventListener('wheel', () => {
     setTimeout(() => {
         const colorBox = document.querySelector('.color-box');
         const olaText = document.querySelector('.ola');
+        const scrollEl = document.querySelector('.scroll-arrow');
 
 
         colorBox.classList.remove('color-box--opening');
@@ -9,5 +10,7 @@ window.addEventListener('wheel', () => {
         
         olaText.classList.remove('ola--show');
         olaText.classList.add('ola--close');
+
+        scrollEl.classList.add('u-close');
     }, 1000)
 });
