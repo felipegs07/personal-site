280270ee418d9475d8a9400d5e4a59c287ac733d
Felipe <felipegdas07@gmail.com>
2019-02-07 16:54:41 -0200

    btn default done, portfolio page done

--- version3/flowing/javascript/portfolio.js
+++ version3/flowing/javascript/portfolio.js
@@ -22,10 +22,10 @@ class portfolio {
 
     createMarkupProjects(projects){
         return projects.map(project => `
-            <article class="project-item">
+            <article class="project-item project-item--white">
                 <img class="project-item__image" src="${project.image}">
                 <h5 class="font-xs project-item__title">${project.title}</h5>
-                <a href="${project.link}" class="btn-project" target="_blank">ver projeto</a>
+                <a href="${project.link}" class="btn btn--secondary" target="_blank">ver projeto</a>
             </article>`).join('');
     } 
 }
@@ -33,3 +33,5 @@ class portfolio {
 document.addEventListener('DOMContentLoaded', () => {
     new portfolio();
 });
+
+//<a href="${project.link}" class="btn-project" target="_blank">ver projeto</a>
