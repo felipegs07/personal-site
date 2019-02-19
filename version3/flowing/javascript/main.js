3b353baf1c26a63a1d3ae766647e816d1d9a0d9a
Felipe <felipegdas07@gmail.com>
2019-02-19 11:42:29 -0300

    google analytics added

--- version3/flowing/javascript/main.js
+++ version3/flowing/javascript/main.js
@@ -33,4 +33,4 @@ function toggleMenu(){
 }
 
 menuBtn.addEventListener('click', toggleMenu);
-console.log('personal-site v3 beta 3');
+console.log('personal-site v3 beta 4 - The one with analytics');
